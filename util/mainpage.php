7d855b86461b68a80000505f0fed7f9fda615aaf
anassghazi567@gmail.com <anassghazi567@gmail.com>
2024-04-28 14:02:04 +0100

    pfe

--- util/mainpage.php
+++ util/mainpage.php
@@ -6,86 +6,136 @@ include("connection.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-     <title>Document</title>
-     <style>
-      .barre_de_recherche{
-        height:220px;
-        width: 1050px;
-       padding: 100px;
-       margin: 250px;
-       margin-top: 130px;
-       padding-top: 50px;
-       background-color: #083344;
-       border-radius: 12px;
-       position: relative;
-      }
-      .input{
-        height: 60px;
-        width: 200px;
-        margin-top: 24px;
-        text-align: center;
-        margin-right: 6px;
-        border-radius: 8px;
-        font-size: 130%;
+    <title>Document</title>
+    <style>
+        .barre_de_recherche {
+            height: 220px;
+            width: 1050px;
+            padding: 100px;
+            margin: 250px;
+            margin-top: 130px;
+            padding-top: 50px;
+            background-color: #083344;
+            border-radius: 12px;
+            position: relative;
+        }
+        .input {
+            height: 60px;
+            width: 200px;
+            margin-top: 24px;
+            text-align: center;
+            margin-right: 6px;
+            border-radius: 8px;
+            font-size: 130%;
+        }
+        .button {
+            background-color: black;
+            color: white;
+        }
+        .card{
+          background-color: #083344;
+          width: 150px;
+          color: white;
+          margin: 350px;
+          
+        }
         
-      }
-      .button{
-        background-color: black;
-        color: white;
-      }
-      </style>
-
+    </style>
 </head>
 <body>
-  <nav class="navbar navbar-light bg-cover bg-cyan-950 ">
-  <div class="container-fluid ">
-    <a class="navbar-brand text-white" href="http://localhost:9000/util/mainpage.php">LOGO</a>
-    <a href="http://localhost:9000/login.php" class="btn btn-dark text-white border border-light">Se connecter</a>
-  </div>
+<nav class="navbar navbar-light bg-cover bg-cyan-950">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white" href="http://localhost:9000/util/mainpage.php">LOGO</a>
+        <a href="http://localhost:9000/login.php" class="btn btn-dark text-white border border-light">Se connecter</a>
+    </div>
 </nav>
-<div>
+
 <div class="barre_de_recherche">
-  <form action="" method="post">
-  <input type="text" placeholder="nom" name="search" class="input">
-  <select name="" id="" class="input">
-  <option value="specialite">specialite</option>
-    <option value="Chiropracteur">Chiropracteur</option>
-    <option value="Diabétologue">Diabétologue</option>
-    <option value="Dentiste">Dentiste</option>
-    <option value="Dermatologue">Dermatologue</option>
-  </select>
-  <select name="" id="" class="input">
-  <option value="ville">ville</option>
-    <option value="casablanca">casablanca</option>
-    <option value="agadir">agadir</option>
-    <option value="fez">fez</option>
-    <option value="tanger">tanger</option>
-  </select>
-  <input type="submit" placeholder="search"  name="submit-search" class="input button" >
-  </form>
-</div>
+    <form action="" method="post">
+        <input type="text" placeholder="Nom" name="search-nom" class="input">
+        <select name="search-specialite" class="input">
+            <option value="">Choisir une spécialité</option>
+            <option value="Chiropracteur">Chiropracteur</option>
+            <option value="Diabétologue">Diabétologue</option>
+            <option value="Dentiste">Dentiste</option>
+            <option value="Dermatologue">Dermatologue</option>
+        </select>
+        <select name="search-ville" class="input">
+            <option value="">Choisir une ville</option>
+            <option value="casablanca">Casablanca</option>
+            <option value="agadir">Agadir</option>
+            <option value="fez">Fez</option>
+            <option value="tanger">Tanger</option>
+        </select>
+        <input type="submit" value="Search" name="submit-search" class="input button">
+    </form>
 </div>
 
 <div>
 <?php
- $sql="SELECT * FROM medecin";
- $result=mysqli_query($conn,$sql);
- $queryResults=mysqli_num_rows($result);
- if($queryResults > 0){
-  while($row=mysqli_fetch_assoc($result)){
-    echo "<div>
-               <h3>".$row['nom']."</h3>
-               <p>".$row['prenom']."</p>
-               <p>".$row['specialite']."</p>
-               <p>".$row['ville']."</p>
-         </div>";
-  }
- }
+if (isset($_POST['submit-search'])) {
+    $nom = mysqli_real_escape_string($conn, $_POST['search-nom']);
+    $specialite = mysqli_real_escape_string($conn, $_POST['search-specialite']);
+    $ville = mysqli_real_escape_string($conn, $_POST['search-ville']);
+
+    $query = "SELECT * FROM medecin WHERE 1=1";
+    $conditions = [];
+    $params = [];
+    $types = "";
+
+    if (!empty($nom)) {
+        $conditions[] = "nom LIKE CONCAT('%', ?, '%')";
+        $params[] = $nom;
+        $types .= "s";
+    }
+    if (!empty($specialite) && $specialite != "") {
+        $conditions[] = "specialite = ?";
+        $params[] = $specialite;
+        $types .= "s";
+    }
+    if (!empty($ville) && $ville != "") {
+        $conditions[] = "ville = ?";
+        $params[] = $ville;
+        $types .= "s";
+    }
+
+    if (!empty($conditions)) {
+        $query .= " AND " . implode(' AND ', $conditions);
+        $stmt = mysqli_prepare($conn, $query);
+        if ($stmt && !empty($params)) {
+            mysqli_stmt_bind_param($stmt, $types, ...$params);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if (mysqli_num_rows($result) > 0) {
+                echo "<div class='row'>";
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<div class='col-md-4 mb-4'>
+                     <div class='card'>
+                     <div class='card-body'>
+                     <h5 class='card-title'>".$row['nom']."</h5> 
+                     <h6 class='card-subtitle mb-2 text-muted'>".$row['prenom']."</h6> 
+                     <p class='card-text'>Spécialité: ".$row['specialite']."</p>
+                     <p class='card-text'>Ville: ".$row['ville']."</p>
+                     </div>
+                     </div>
+                     </div>";
+                }
+                echo "</div>"; // row
+            } else {
+                echo "No results matching your search!";
+            }
+        }
+    } else {
+        echo "Please enter search criteria!";
+    }
+}
 ?>
 </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
--- util/recherche.php
+++ util/recherche.php
@@ -1,6 +1,7 @@
 <?php
 include("connection.php");
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,45 +9,19 @@ include("connection.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Search Medecin</title>
 </head>
 <body>
 <nav class="navbar navbar-light bg-cover bg-cyan-950 ">
-  <div class="container-fluid ">
+  <div class="container-fluid">
     <a class="navbar-brand text-white" href="http://localhost:9000/util/mainpage.php">LOGO</a>
     <a href="http://localhost:9000/login.php" class="btn btn-dark text-white border border-light">Se connecter</a>
   </div>
 </nav>
-7
-<hI>Search page</hl> 
-<div class="article-container">
-<?php
-if(isset($_POST['submit-search'])){
-$search = mysqli_real_escape_string($conn, $_POST[ 'search']);
-$sql="SELECT * FROM medecin WHERE nom  LIKE '%search%' 
-      OR prenom LIKE '%search%' 
-      OR specialite LIKE '%search%' 
-      OR ville LIKE '%search%' ";
-$result=mysqli_query($conn, $sql);
-$queryResu1t=mysqli_num_rows($result);
-echo "there are ".$queryResults."results!";
-if($queryResults > 0){
-  while($row=mysqli_fetch_assoc($result)){
-    echo "<div>
-               <h3>".$row['nom']."</h3>
-               <p>".$row['prenom']."</p>
-               <p>".$row['specialite']."</p>
-               <p>".$row['ville']."</p>
-         </div>";
-     }
-   }else{
-    echo "there are no results matching your search!";
-   }
- }
- ?>
- </div>
 
 
+
+</div>
 
 </body>
 </html>
